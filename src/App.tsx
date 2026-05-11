0cb7b8154d60a2590cff82ef0ec69eec7d152213
MohamedFowzey <fowzeym@gmail.com>
2026-05-11 18:28:44 +0300

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './Layouts/AuthLayout/AuthLayout'
+import NotFound from './Modules/Shared/NotFound/NotFound'
+import Login from './Modules/Athentication/Login/Login'
+import MasterLayout from './Layouts/MasterLayout/MasterLayout'
 
 function App() {
+  const routes = createBrowserRouter([{
+    path:'/',element:<AuthLayout/>,
+    errorElement:<NotFound/>,
+    children:[
+      {index:true,element:<Login/>},
+      {path:'login',element:<Login/>},
+    ]
+  }
+  ,{path:'dashboard',element:<MasterLayout/>,children:[
 
-  return (
-    <>
-    <h1>hello world</h1>
-    </>
-  )
+  ]}
+])
+  return <RouterProvider router={routes}/>
 }
 
 export default App
