a38fcade1f4889b75763e44ae5b898108cf2e976
tonefarhoud3433-ai <tonefarhoud3433@gmail.com>
2026-05-13 00:08:52 +0300

    handle auth layout

--- src/Layouts/AuthLayout/AuthLayout.tsx
+++ src/Layouts/AuthLayout/AuthLayout.tsx
@@ -1,10 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import authLogo from "../../assets/Images/authLogo.png";
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout() {
   const { pathname } = useLocation();
 
   const getBgClass = () => {
@@ -17,9 +13,14 @@ export default function AuthLayout({
     return "";
   };
 
+  const getClassName = () => {
+    if (pathname === "/register") return "w-1/2";
+    else return "w-1/3";
+  };
+
   return (
-    <div className={`${getBgClass()} text-auth`}>
-      <div className="flex flex-col items-center justify-center h-full ">
+    <div className={`${getBgClass()} text-auth h-full px-5`}>
+      <div className="flex flex-col items-center justify-center h-dvh">
         <div className="mb-3">
           <img
             src={authLogo}
@@ -27,7 +28,9 @@ export default function AuthLayout({
             className="w-40 md:w-52 lg:w-72"
           />
         </div>
-        <div className="bg-main-light-color w-2/3 md:w-1/2 lg:w-1/3  rounded-lg shadow-lg py-28 px-14">
+        <div
+          className={`bg-main-light-color w-full md:w-2/3 lg:w-1/2 xl:${getClassName()} rounded-lg shadow-lg py-28 px-14 overflow-scroll height-scroll-bar`}
+        >
           <p>welcome to PMS</p>
           <Outlet />
         </div>
