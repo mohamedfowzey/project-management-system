369ebdaf858c030cab74a64629a469eef488aba0
tonefarhoud3433-ai <tonefarhoud3433@gmail.com>
2026-05-12 21:20:08 +0300

    add links

--- src/Modules/Athentication/ForgetPassword/ForgetPassword.tsx
+++ src/Modules/Athentication/ForgetPassword/ForgetPassword.tsx
@@ -1,50 +1,55 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import CustomInput from '../../Shared/CustomInput/CustomInput';
-import CustomButton from '../../Shared/CustomButton/CustomButton';
-
+import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import CustomInput from "../../Shared/CustomInput/CustomInput";
+import CustomButton from "../../Shared/CustomButton/CustomButton";
 
 export interface forgetData {
-  email: string,
-
+  email: string;
 }
 
 export default function ForgetPassword() {
-
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors } } = useForm<forgetData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<forgetData>();
 
   const onsubmit = async (data: forgetData) => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const response = await axios.post("https://upskilling-egypt.com:3003/api/v1/Users/Reset/Request", data);
+      const response = await axios.post(
+        "https://upskilling-egypt.com:3003/api/v1/Users/Reset/Request",
+        data,
+      );
 
       toast.success(response?.data?.message);
-      navigate('/reset-password')
-
+      navigate("/reset-password");
     } catch (error: any) {
-      toast.error(error.response?.data?.message)
-
+      toast.error(error.response?.data?.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
-
+  };
 
   return (
     <>
-      <form className='my-3.5' onSubmit={handleSubmit(onsubmit)}>
-
+      <form className="my-3.5" onSubmit={handleSubmit(onsubmit)}>
         <CustomInput
           register={register("email", { required: "Email is required" })}
           HTMLtype="email"
           label="E-mail"
           error={errors.email?.message}
         />
+        <div className="links flex justify-end my-3">
+          <Link className="text-muted text-decoration-none" to="/login">
+            Login Now?
+          </Link>
+        </div>
         <CustomButton
           text="Verify"
           loading={loading}
@@ -52,9 +57,6 @@ export default function ForgetPassword() {
           onClick={handleSubmit(onsubmit)}
         />
       </form>
-
-
-
     </>
-  )
+  );
 }
