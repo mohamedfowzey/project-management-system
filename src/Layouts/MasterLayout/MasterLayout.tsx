4f7351479cdfc25b6560c15a686a5600286e0484
tonefarhoud3433-ai <tonefarhoud3433@gmail.com>
2026-05-16 13:52:36 +0300

    feat(layout): setup MasterLayout structure with flexbox

--- src/Layouts/MasterLayout/MasterLayout.tsx
+++ src/Layouts/MasterLayout/MasterLayout.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import NavBar from "../../Modules/Shared/NavBar/NavBar";
+import SideBar from "../../Modules/Shared/SideBar/SideBar";
 
 export default function MasterLayout() {
   return (
-    <div>MasterLayout</div>
-  )
+    <>
+      <div className="flex flex-col min-h-screen">
+        <div className="w-full">
+          <NavBar />
+        </div>
+        <div className="flex flex-1">
+          <div className="shrink-0">
+            <SideBar />
+          </div>
+          <div className="flex-1 min-w-0">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
