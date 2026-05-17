f3f26888aa509853ed66f1afb4eea80aa6e97c87
MohamedFowzey <fowzeym@gmail.com>
2026-05-18 02:26:39 +0300

    center form vertically

--- src/Modules/Dashboard/ProjectDataForm/ProjectDataForm.tsx
+++ src/Modules/Dashboard/ProjectDataForm/ProjectDataForm.tsx
@@ -1,10 +1,9 @@
-import type { JSX } from "react";
 
 export default function ProjectDataForm() {
   return (
     <>
-      <div className="h-[calc(100vh = 80px)] self-stretch flex justify-center items-center">
-        <div className="rounded-xl bg-white dark:bg-gray-950 w-full md:w-[90%] lg:w-80 mx-auto py-10 px-5">
+      <div className="h-[calc(100vh-80px)] self-stretch flex justify-center items-center">
+        <div className="rounded-3xl bg-white dark:bg-gray-950 w-full md:w-[90%] lg:w-80 mx-auto py-10 px-5">
           <div className="mb-4">
             <span className="text-sm font-medium text-gray-700 dark:text-gray-200">
               {" "}
@@ -25,6 +24,9 @@ export default function ProjectDataForm() {
               placeholder="description"
               className="mt-0.5 w-full rounded-2xl p-2 border-1 focus-visible:outline-0  border-gray-300 shadow-sm md:text-xl dark:shadow-sm shadow-gray-600 dark:border-gray-600 dark:bg-gray-900 dark:text-white"
             ></textarea>
+            <div className="flex justify-between">
+
+            </div>
           </div>{" "}
         </div>
       </div>
