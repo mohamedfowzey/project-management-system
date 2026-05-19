377844d61f6bb8d72377d4860dbc316b6bd94371
tonefarhoud3433-ai <tonefarhoud3433@gmail.com>
2026-05-19 15:27:36 +0300

    finish side-bar

--- src/Modules/Shared/SideBar/SideBar.tsx
+++ src/Modules/Shared/SideBar/SideBar.tsx
@@ -1,27 +1,64 @@
-import { Users, Briefcase, SquareCheck, LogOut } from "lucide-react";
+import { AuthContext } from "../../../Contexts/AuthContext";
+import {
+  Users,
+  Briefcase,
+  SquareCheck,
+  LogOut,
+  ChevronRight,
+  ChevronLeft,
+} from "lucide-react";
+import { useContext, useState } from "react";
 import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
 
 export default function SideBar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const auth = useContext(AuthContext);
+  if (!auth) return null;
+  const { logOut } = auth;
   return (
-    <Sidebar>
-      <Menu>
-        <MenuItem icon={<Users size={18} />}>
-          <Link to={"users"}>Users</Link>
-        </MenuItem>
+    <div className="sidebar-container relative h-full">
+      <button
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className="toggle-sidebar-btn absolute top-[1%] left-full z-50 flex h-16 w-8 items-center justify-center rounded-r-xl transition-all duration-300"
+        style={{
+          backgroundColor: "rgba(239, 155, 40, 1)", // اللون البرتقالي من الديزاين
+          color: "white",
+          cursor: "pointer",
+          border: "none",
+        }}
+      >
+        {isCollapsed ? <ChevronRight size={18} /> : <ChevronLeft size={18} />}
+      </button>
+      <Sidebar collapsed={isCollapsed}>
+        <Menu>
+          <MenuItem icon={<Users size={18} />} component={<Link to="users" />}>
+            Users
+          </MenuItem>
 
-        <MenuItem icon={<Briefcase size={18} />}>
-          <Link to={"projects"}>Projects</Link>
-        </MenuItem>
+          <MenuItem
+            icon={<Briefcase size={18} />}
+            component={<Link to="projects" />}
+          >
+            Projects
+          </MenuItem>
 
-        <MenuItem icon={<SquareCheck size={18} />}>
-          <Link to={"tasks"}>Tasks</Link>
-        </MenuItem>
+          <MenuItem
+            icon={<SquareCheck size={18} />}
+            component={<Link to="tasks" />}
+          >
+            Tasks
+          </MenuItem>
 
-        <MenuItem icon={<LogOut size={18} />} style={{ marginTop: "20px" }}>
-          Logout
-        </MenuItem>
-      </Menu>
-    </Sidebar>
+          <MenuItem
+            onClick={() => logOut()}
+            icon={<LogOut size={18} />}
+            style={{ marginTop: "20px" }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </div>
   );
 }
