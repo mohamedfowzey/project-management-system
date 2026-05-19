8bf92f8cc3520e6ab20a86ea947967bbc552cfaf
MohamedFowzey <fowzeym@gmail.com>
2026-05-19 19:43:54 +0300

    fine taskslist

--- src/Modules/Dashboard/Tasks/Tasks.tsx
+++ src/Modules/Dashboard/Tasks/Tasks.tsx
@@ -3,6 +3,7 @@ import { ProjectsApi, Tasks } from "../../../api";
 import { useNavigate } from "react-router-dom";
 import CustomButton from "../../Shared/CustomButton/CustomButton";
 import {
+  ArrowUpZA,
   ChevronLeft,
   ChevronRight,
   Eye,
@@ -12,17 +13,17 @@ import {
 } from "lucide-react";
 import type { User } from "../../../api/modules/user";
 import type { Project } from "../../../api/modules/Projects";
+import NoData from "../../Shared/NoData/NoData";
 // "../../Modules/Shared/NoData/NoData";
 
 interface Task {
   id: number;
   title: string;
-  description:string;
+  description: string;
   status: string;
-employee:User,
-  project:Project,
+  employee: User;
+  project: Project;
   creationDate: string;
-  
 }
 // https://upskilling-egypt.com:3003/api/v1/Task/manager
 export default function Projects() {
@@ -34,7 +35,7 @@ export default function Projects() {
   const [pageSize, setPageSize] = useState(10);
   const [totalResults, setTotalResults] = useState(0);
 
-  const navigate = useNavigate() 
+  const navigate = useNavigate();
 
   const fetchProjects = async () => {
     try {
@@ -94,74 +95,77 @@ export default function Projects() {
       )
     : projects;
   useEffect(() => {
-    const delay = setTimeout(()=>{
-
+    const delay = setTimeout(() => {
       fetchProjects();
-    },500);
-    
-    return ()=> clearTimeout(delay)
+    }, 500);
+
+    return () => clearTimeout(delay);
   }, [searchTerm, currentPage, pageSize]);
 
   return (
     <>
-    <div className="flex justify-between items-center mt-2 mb-10 py-4 px-2 md:px-9.5 bg-white dark:bg-gray-950 ">
+      <div className="flex justify-between items-center mt-2 mb-10 py-4 px-2 md:px-9.5 bg-white dark:bg-gray-950 ">
         <h1>Tasks</h1>
-        <div className="shrink mt-[-1rem]" onClick={()=>navigate('/dashboard/add-task')}>
-    
-        <CustomButton text=" + add Task " />
+        <div
+          className="shrink mt-[-1rem]"
+          onClick={() => navigate("/dashboard/add-task")}
+        >
+          <CustomButton text=" + add Task " />
         </div>
       </div>
-    <div className="table-wrapper">
-      <div className="search-filter-container">
-        <div className="search-wrapper  ">
-          <span className="search-icon">
-            <Search size={20} strokeWidth={1.75} />
-          </span>
-          <input
-            type="text"
-            placeholder="Search By Title"
-            className="search-input"
-            value={searchTerm}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
+      <div className="table-wrapper">
+        <div className="search-filter-container">
+          <div className="search-wrapper  ">
+            <span className="search-icon">
+              <Search size={20} strokeWidth={1.75} />
+            </span>
+            <input
+              type="text"
+              placeholder="Search By Title"
+              className="search-input"
+              value={searchTerm}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
-      {loading ? (
-        <div className=" text-center p-5">Loading...</div>
-      ) : (
-        <>
-          <div className="table-container">
-            <table className="data-table">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Description</th>
-                  <th>Status</th>
-                  <th>User</th>
-                  <th>Project</th>
-                  <th>Date Created</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  filteredProjects.map((project) => (
-                    <tr key={project?.id} className="table-row">
-                      <td className=" dark:text-black ">{project.title}</td>
-                      <td className=" dark:text-black ">{project?.description}</td>
+        {loading ? (
+          <div className=" text-center p-5">Loading...</div>
+        ) : (
+          <>
+            <div className="table-container">
+              <table className="data-table">
+                <thead>
+                  <tr className="bg-emerald-800 text-white dark:bg-gray-700">
+                    <th className="flex gap-1 items-center ">
+                      Title{" "}
+                      <ArrowUpZA
+                        size={20}
+                        strokeWidth={1.5}
+                        absoluteStrokeWidth
+                      />
+                    </th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>User</th>
+                    <th>Project</th>
+                    <th>Date Created</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                  {filteredProjects.length > 0 ? (
+                  filteredProjects.map((task) => (
+                    <tr key={task?.id} className="table-row dark:bg-taupe-900">
+                      <td className="">{task.title}</td>
+                      <td className="">{task?.description}</td>
                       <td>
-                        <span className="status-badge ">{project.status}</span>
+                        <span className="status-badge bg-emerald-800 text-white dark:bg-gray-700">{task.status}</span>
                       </td>
-                      <td className=" dark:text-black ">
-                        {project.
-employee
-?.userName}
-                      </td>
-                      <td className=" dark:text-black ">
-                        {project?.project?.title}
-                      </td>
-                      <td className=" dark:text-black ">
-                        {new Date(project.creationDate).toLocaleDateString()}
+                      <td className="">{task.employee?.userName}</td>
+                      <td className="">{task?.project?.title}</td>
+                      <td className="">
+                        {new Date(task.creationDate).toLocaleDateString()}
                       </td>
                       <td className="actions-cell">
                         <div className="actions-wrapper">
@@ -169,15 +173,15 @@ employee
                             className="menu-btn"
                             onClick={() =>
                               setOpenMenu(
-                                openMenu === project.id ? null : project.id,
+                                openMenu === task.id ? null : task.id,
                               )
                             }
                           >
                             ⋮
                           </button>
-                          {openMenu === project.id && (
-                            <div className="actions-menu">
-                              <button className="action-btn view-btn  dark:text-black ">
+                          {openMenu === task.id && (
+                            <div className="actions-menu  bg-amber-50  dark:bg-gray-400">
+                              <button className="action-btn view-btn  dark:text-gray-700 ">
                                 <Eye
                                   color="var(--bg-main-color)"
                                   size={20}
@@ -186,7 +190,14 @@ employee
                                 />{" "}
                                 View
                               </button>
-                              <button className="action-btn edit-btn  dark:text-black" onClick={()=>navigate(`/dashboard/edit-task/${project?.id}`)}>
+                              <button
+                                className="action-btn edit-btn dark:text-emerald-900"
+                                onClick={() =>
+                                  navigate(
+                                    `/dashboard/edit-task/${task?.id}`,
+                                  )
+                                }
+                              >
                                 <FilePenLine
                                   color="var(--bg-main-color)"
                                   size={20}
@@ -197,7 +208,7 @@ employee
                               </button>
                               <button
                                 className="action-btn delete-btn dark:text-black"
-                                onClick={() => handleDelete(project.id)}
+                                onClick={() => handleDelete(task.id)}
                               >
                                 <Trash2
                                   color="var(--bg-main-color)"
@@ -213,53 +224,71 @@ employee
                       </td>
                     </tr>
                   ))
-                }
-              </tbody>
-            </table>
-          </div>
+                  ) : (
+                                      <tr>
+                                        <td colSpan={6} className="text-center dark:text-black">
+                                          <NoData />
+                                        </td>
+                                      </tr>
+                                    )}
+                </tbody>
+              </table>
+            </div>
 
-          <div className="pagination">
-            <div className="pagination-info">
-              <span>Showing</span>
-              <select
-                value={pageSize}
-                onChange={(e) => handlePageSizeChange(Number(e.target.value))}
-                className="page-size-select"
-              >
-                <option value={5}>5</option>
-                <option value={10}>10</option>
-                <option value={15}>15</option>
-                <option value={20}>20</option>
-              </select>
-              <span>of {totalResults} Results</span>
-              <span className="ml-4">
-                Page {currentPage} of {Math.ceil(totalResults / pageSize)}
-              </span>
+            <div className="pagination">
+              <div className="pagination-info">
+                <span>Showing</span>
+                <select
+                  value={pageSize}
+                  onChange={(e) => handlePageSizeChange(Number(e.target.value))}
+                  className="page-size-select bg-white dark:bg-black "
+                >
+                  <option className=" dark:text-gray-300" value={5}>
+                    5
+                  </option>
+                  <option className=" dark:text-gray-300" value={10}>
+                    10
+                  </option>
+                  <option className=" dark:text-gray-300" value={15}>
+                    15
+                  </option>
+                  <option className=" dark:text-gray-300" value={20}>
+                    20
+                  </option>
+                </select>
+                <span>of {totalResults} Results</span>
+                <span className="ml-4">
+                  Page {currentPage} of {Math.ceil(totalResults / pageSize)}
+                </span>
+              </div>
+              <div className="pagination-controls">
+                <button
+                  className="page-btn"
+                  onClick={handlePrevPage}
+                  disabled={currentPage === 1}
+                >
+                  <ChevronLeft
+                    size={20}
+                    strokeWidth={1.5}
+                    absoluteStrokeWidth
+                  />{" "}
+                </button>
+                <button
+                  className="page-btn"
+                  onClick={handleNextPage}
+                  disabled={currentPage >= Math.ceil(totalResults / pageSize)}
+                >
+                  <ChevronRight
+                    size={20}
+                    strokeWidth={1.5}
+                    absoluteStrokeWidth
+                  />
+                </button>
+              </div>
             </div>
-            <div className="pagination-controls">
-              <button
-                className="page-btn"
-                onClick={handlePrevPage}
-                disabled={currentPage === 1}
-              >
-                <ChevronLeft
-                  size={20}
-                  strokeWidth={1.5}
-                  absoluteStrokeWidth
-                />{" "}
-              </button>
-              <button
-                className="page-btn"
-                onClick={handleNextPage}
-                disabled={currentPage >= Math.ceil(totalResults / pageSize)}
-              >
-                <ChevronRight size={20} strokeWidth={1.5} absoluteStrokeWidth />
-              </button>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
