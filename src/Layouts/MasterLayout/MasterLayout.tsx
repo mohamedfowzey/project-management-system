7038ce095966d973dac83ccb55a6cf4c2b61aa27
MohamedFowzey <fowzeym@gmail.com>
2026-05-20 09:42:33 +0300

    ui improvement

--- src/Layouts/MasterLayout/MasterLayout.tsx
+++ src/Layouts/MasterLayout/MasterLayout.tsx
@@ -11,11 +11,11 @@ export default function MasterLayout() {
   return (
     <>
       <div className={`flex flex-col min-h-screen bg-gray-50 ${mood} dark:bg-gray-900 dark:text-gray-50`}>
-        <div className="w-full">
+        <div className="w-full sticky top-0 z-10">
           <NavBar />
         </div>
-        <div className="flex flex-1">
-          <div className="shrink-0">
+        <div className="flex flex-1 overflow-visible">
+          <div className="shrink-0 sticky top-20 h-[calc(100vh-80px)]">
             <SideBar />
           </div>
           <div className="flex-1 min-w-0 bg-gray-50 dark:bg-gray-900">
